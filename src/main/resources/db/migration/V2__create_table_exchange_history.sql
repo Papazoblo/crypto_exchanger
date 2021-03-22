5eae2e3e5146f9726f0f62028aa7d5ed1794dc85
Alexey Medvedev <wwwalex_96@mail.ru>
2021-03-22 22:09:24 +0300

    feat: добавлена миграция для таблицы истории обмена

---
+++ src/main/resources/db/migration/V2__create_table_exchange_history.sql
@@ -0,0 +1,11 @@
+CREATE TABLE exchange_history (
+    id               bigint PRIMARY KEY NOT NULL,
+    order_id         bigint             NOT NULL,
+    type             varchar(20)        NOT NULL,
+    datetime         timestamp          NOT NULL,
+    initial_amount   varchar(100)       NOT NULL,
+    final_amount     varchar(100)       NOT NULL,
+    private          varchar(100)       NOT NULL,
+    order_status     varchar(30)        NOT NULL,
+    id_prev_exchange bigint
+);
