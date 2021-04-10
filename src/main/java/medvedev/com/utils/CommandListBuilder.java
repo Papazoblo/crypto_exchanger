44c7f5eadb0fc1a767308e0dd4fefff97c1d0181
Alexey Medvedev <wwwalex_96@mail.ru>
2021-04-10 21:52:19 +0300

    feat: добавлен вывод списка доступных команд

---
+++ src/main/java/medvedev/com/utils/CommandListBuilder.java
@@ -0,0 +1,26 @@
+package medvedev.com.utils;
+
+import lombok.experimental.UtilityClass;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+
+import java.util.Arrays;
+import java.util.List;
+
+@UtilityClass
+public class CommandListBuilder {
+
+    public static List<BotCommand> getCommandList() {
+        return Arrays.asList(
+                createCommand("/start", "Authenticated command"),
+                createCommand("/launched", "Launch service"),
+                createCommand("/stopped", "Stop service")
+        );
+    }
+
+    private static BotCommand createCommand(String commandName, String commandDescription) {
+        return BotCommand.builder()
+                .command(commandName)
+                .description(commandDescription)
+                .build();
+    }
+}
