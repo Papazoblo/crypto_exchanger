d1908471f7325d504ad1ceeae68b8b613266bdbd
Alexey Medvedev <wwwalex_96@mail.ru>
2021-04-11 18:07:46 +0300

    fix: исправлена ошибка при формировании исключения

--- src/main/java/medvedev/com/exception/NotEnoughFundsBalanceException.java
+++ src/main/java/medvedev/com/exception/NotEnoughFundsBalanceException.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class NotEnoughFundsBalanceException extends RuntimeException {
 
     public NotEnoughFundsBalanceException(BigDecimal balance) {
-        super(String.format("Not enough funds on the balance. Balance = %d", balance.doubleValue()));
+        super(String.format("Not enough funds on the balance. Balance = %d", balance.intValue()));
     }
 }
